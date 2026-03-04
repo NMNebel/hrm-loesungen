17ba9ee150a4196c8afab251c0c7d06cb7a1f3f4
Nico Nebel <git@niconebel.de>
2026-03-04 23:11:03 +0100

    Initial check in

---
+++ loesungen/Jahr_04_Verwirrungsverwalter.asm
@@ -0,0 +1,11 @@
+-- Verwirrungsverwalter
+-- Befehle 7/7 - Schritte 21/21
+
+a:
+    inbox
+    copyto 0
+    inbox
+    outbox
+    copyfrom 0
+    outbox
+    jump     a
---
+++ loesungen/Jahr_05_Nullenvernichter.asm
@@ -0,0 +1,9 @@
+-- Nullenvernichter
+-- Befehle 4/4 - Schritte 23/23
+
+a:
+b:
+    inbox
+    jumpz    b
+    outbox
+    jump     a
