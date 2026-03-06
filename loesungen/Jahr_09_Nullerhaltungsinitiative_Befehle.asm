6e0fdf6ae0bb61a8212b4fce7c47017e92fcdabf
Nico Nebel <git@niconebel.de>
2026-03-06 19:37:56 +0100

    In zwei Programm aufgeteilt

--- loesungen/Jahr_09_Nullerhaltungsinitiative_Befehle.asm
+++ loesungen/Jahr_09_Nullerhaltungsinitiative_Befehle.asm
@@ -1,4 +1,4 @@
-;   Nullenvernichter
+;   Nullenvernichter, Leistungsoptimierung Befehle
 
 a:
 b:
---
+++ loesungen/Jahr_09_Nullerhaltungsinitiative_Schritte.asm
@@ -0,0 +1,17 @@
+;   Nullenvernichter, Leistungsoptimierung Schritte
+
+a:
+b:
+c:
+    inbox
+    jumpz    d
+    jump     b
+d:
+    outbox
+;   naechsten Wert einlesen
+    inbox
+    jumpz    e
+    jump     c
+e:
+    outbox
+    jump     a
